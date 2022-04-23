4f7f4b6afbf057565a28a3338c94ffb0aa3b4bd4
Sebastian Ober <sebastian.ober@posteo.de>
2022-04-24 00:57:18 +0200

    remove old benchmark numbers

--- src/products.rs
+++ src/products.rs
@@ -4,21 +4,21 @@ use crate::spheres::Spheres;
 
 #[derive(Debug, FromPrimitive, Copy, Clone)]
 pub enum ProductRecipes {
-    NaquiumTesseract1 = 0, // > 12
-    NaquiumTesseract2 = 1, // > 11
-    WormholeData = 2,      // [2, 4, 6, 8]
-    NaquiumProcessor1 = 3, // > 11
-    NaquiumProcessor2 = 4, // > 10
-    SpaceDilation1 = 5,    // [1, 3, 4, 4, 5, 7]
-    SpaceDilation2 = 6,    // [2, 4, 6, 7, 7, 8]
-    SpaceFolding1 = 7,     // [5, 6, 7, 8]
-    SpaceFolding2 = 8,     // > 10
-    SpaceInjection1 = 9,   // [1, 3, 5, 7, 8, 8]
-    SpaceInjection2 = 10,  // > 10
-    SpaceWarping1 = 11,    // [3, 4, 5, 6]
-    SpaceWarping2 = 12,    // [1, 2, 3, 4]
-    Singularity1 = 13,     // [3, 7]
-    Singularity2 = 14,     // [2, 6]
+    NaquiumTesseract1 = 0,
+    NaquiumTesseract2 = 1,
+    WormholeData = 2,
+    NaquiumProcessor1 = 3,
+    NaquiumProcessor2 = 4,
+    SpaceDilation1 = 5,
+    SpaceDilation2 = 6,
+    SpaceFolding1 = 7,
+    SpaceFolding2 = 8,
+    SpaceInjection1 = 9,
+    SpaceInjection2 = 10,
+    SpaceWarping1 = 11,
+    SpaceWarping2 = 12,
+    Singularity1 = 13,
+    Singularity2 = 14,
 }
 
 pub static PRODUCTS: [Spheres; 15] = [
@@ -118,7 +118,7 @@ pub static PRODUCTS: [Spheres; 15] = [
         omega: 0,
         lambda: -1,
         xi: -1,
-        epsilon:1,
+        epsilon: 1,
         phi: 1,
     },
     Spheres {
